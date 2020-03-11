4c14b52684842f7bb03d1c6b1a03987a6a1c52d7
KushnirUO <kushnir1145@gmail.com>
2020-03-11 09:15:12 +0200

    first commit

--- app/function.php
+++ app/function.php
@@ -1,26 +1,39 @@
 <?php
-require_once "../config/settings.php";
+require_once "../set/settings.php";
 
-function dd($date, $stop=null){
-    echo "<pre>Dump:<br>";
-    print_r($date);
+function dd($data, $stop=null){
+    echo "DUMP:";
+    echo "<pre><br>";
+    print_r($data);
     echo "</pre>";
-    if($stop!=null && $stop!=0)exit();
+    if($stop!=0) exit();
+}
+
+function connect(){
+    $ar = get_param();
+    $dsn= "mysql:dbname=".$ar['db_name'].";host=127.0.0.1";
+    $user=$ar['user'];
+    $password=$ar['password'];
+    try{
+        $link=new PDO($dsn,$user,$password);
+        // echo "success"."<br>";
+    }catch(PDOException $e){
+        echo "some errors".$e->getMessage();
+
+    }
+    return $link;
+
+}
+
+function check($login,$pas,$link){
+    $my_query = "select * from users WHERE name='".$login."' and password='".$pas."'";
+    $ar = $link->query($my_query)->fetchAll(2);
+    return count($ar);
 }
 
 
-function connect(){
-    $ar=get_param();
-
-    dd($ar);
-    $dsn = "mysql: dbname=".$ar['db_name'].";host=127.0.0.1";
-    $user = $ar['user'];
-    $password = $ar['password'];
-    try {
-        $link = new PDO($dsn, $user, $password);
-        echo "success";
-    } catch (PDOException $e){
-        echo "some errors".$e->getMessage();
-    }
-    return $link;
+function cnt_users($login){
+    $link = connect();
+    $ar = $link->query("select * from users where name='".$login."'")->fetchAll(2);
+    return count($ar);
 }
